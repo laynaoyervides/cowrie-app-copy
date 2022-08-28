d0aaa89b559d5d292501717f845dbc1b3c21b717
Layna Oyervides <laynaoyervides@gmail.com>
2022-08-28 16:31:39 -0600

    Installing MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import {Routes, Route} from "react-router-dom"
 
-import './App.css';
+//Material UI
+import {createTheme, ThemeProvider} from '@mui/material/styles'
+import{purple, deepOrange} from 'material-ui-colors';
+
+//import subComponents
 import NavBar from "./Nav_Bar";
 import Login from "./Login"
 import About from './About';
@@ -12,26 +16,40 @@ import Art from './Art';
 import Nfts from './Nfts';
 
 function App() {
+
+// set theme colors to purple primary: #9c27b0, L=6d1b7b, d=af52bf
+//secondary color is deepOrange: main:ff3d00, l ff6333, d: b22a00
+//color of interest: #ffc9b7 (peach)
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: deepOrange[400],
+    },
+  },
+});
+
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
       <NavBar />
       <Routes>
-      <Route path="/about" element={<About />} />
-        <Route path="/learners" element={<Learners />} />
-        <Route path="/tutorials" element={<Tutorials />} />
-        <Route path="/market" element={<Market />} />
-        <Route path="/art" element={<Art />} />
-        <Route path="/nfts" element={<Nfts />} />
-        <Route path="/wallet" element={<Wallet />} />
-        <Route path="/login" element={<Login />} />
-
-
-
-
-
-
+           <Route path="/about" element={<About />} />
+          <Route path="/learners" element={<Learners />} />
+          <Route path="/tutorials" element={<Tutorials />} />
+          <Route path="/market" element={<Market />} />
+          <Route path="/art" element={<Art />} />
+          <Route path="/nfts" element={<Nfts />} />
+          <Route path="/wallet" element={<Wallet />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<h1>404 not found</h1>}/>
       </Routes>
     </div>
+    </ThemeProvider>
   );
 }
 
--- client/src/Nav_Bar.js
+++ client/src/Nav_Bar.js
@@ -1,21 +1,32 @@
 import React from "react"
+import {AppBar, Toolbar, Typography} from '@mui/material';
 import {Link} from "react-router-dom"
 
 function NavBar () {
+    
+    
     return (
-        <div>
-            <nav>
-                <Link to="/about">About</Link>
-                <Link to="/wallet">Wallet</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/learners">Learners</Link>
-                <Link to="/tutorials">Tutorials</Link>
-                <Link to="/art">Art</Link>
-                <Link to="/market">Market</Link>
-                <Link to="/nfts">Nfts</Link>
-
-            </nav>
-        </div>
-    )
-}
+           <div>
+                <AppBar>
+                    <Toolbar>
+                        <Typography>
+                            COWRIE
+                          <nav>
+                                <Link to="/about">About</Link>
+                                <Link to="/wallet">Wallet</Link>
+                        <Link to="/login">Login</Link>
+                        <Link to="/learners">Learners</Link>
+                        <Link to="/tutorials">Tutorials</Link>
+                        <Link to="/art">Art</Link>
+                        <Link to="/market">Market</Link>
+                        <Link to="/nfts">Nfts</Link>
+                        </nav>
+                              
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </div>
+          
+    );
+};
 export default NavBar
