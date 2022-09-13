3e738effe75a68fe652ad82054d6ae2a1e7a7970
Layna Oyervides <laynaoyervides@gmail.com>
2022-09-13 07:33:26 -0600

    Setting login inputs in state

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react"
-import { Box, Typography, TextField, Button} from "@mui/material"
+import { Box, Typography, TextField, Button, inputAdornmentClasses} from "@mui/material"
 import AuthAdmin from "./Auth_Admin"
 import AuthUsers from "./Auth_Users"
 import CreateAdmin from "./Create_Admin"
+import { InputSharp } from "@mui/icons-material"
 
 function Login () {
 
         const [isSignup, setIsSignup] =useState(false);
+        const [inputs, setInputs] =useState({
+            
+                name: "",
+                email: "",
+                password: ""
+
+            });
 
     return (
         <div>
@@ -25,13 +33,41 @@ function Login () {
                         boxShadow:'10px 10px 20px #ccc'
                     }}}
                 >
-                <Typography variant="h2" padding={3} textAlign="center">LOGIN</Typography>
+                <Typography 
+                variant="h2" 
+                padding={3} 
+                textAlign="center">
+                    {isSignup ? "SIGNUP" : "LOGIN"}
+        
+                </Typography>
+                
                 {isSignup && (
-                <TextField margin="normal" type={'text'} variant="outlined" placeholder="Name"/>
+                 <TextField onChange={}
+                    name="name"
+                    value={inputs.name}
+                    margin="normal" 
+                    type={'text'} 
+                    variant="outlined" 
+                    placeholder="Name"/>
                 )}
-                <TextField margin="normal" type={'email'} variant="outlined"placeholder="Email"/>
-                <TextField margin="normal" type={'password'} variant="outlined"placeholder="Password"/>
-                <Button variant="contained" color="warning" sx={{marginTop: 3, borderRadius: 3}}>Login</Button>
+
+                <TextField 
+                    name="email"
+                    value={inputs.email}
+                    margin="normal" type={'email'} 
+                    variant="outlined"
+                    placeholder="Email"/>
+                <TextField 
+                    name="password"
+                    value={inputs.password}
+                    margin="normal" 
+                    type={'password'} 
+                    variant="outlined"
+                    placeholder="Password"/>
+                <Button variant="contained" color="warning" sx={{marginTop: 3, borderRadius: 3}}>
+                    {isSignup ? "SIGNUP" : "LOGIN"}
+                    
+                    </Button>
                 <Button 
                 onClick={()=> setIsSignup(!isSignup)} 
                 sx={{ marginTop:3, borderRadius: 3}}
