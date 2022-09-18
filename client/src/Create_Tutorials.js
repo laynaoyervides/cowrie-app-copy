cdaf1e8439db14063d90799f5702667671ad2fcc
Layna Oyervides <laynaoyervides@gmail.com>
2022-09-18 10:36:34 -0600

    Creating CreateTutorials component

--- client/src/Create_Tutorials.js
+++ client/src/Create_Tutorials.js
@@ -1,12 +1,79 @@
+import { Button } from "@mui/material";
 import React from "react"
-import { Typography } from "@mui/material"
 
 
-function CreateTutorials () {
+function CreateTutorials ({tutorials, addNewTutorial }) {
+   const [tutorialTitle, setTutorialTitle]=useState("");
+   const [tutorialDescription, setTutorialDescription]=useState("");
+   const [tutorialTopic, setTutorialTopic]=useState("");
+
+   const newTutorial = {
+        tutorialTitle,
+        tutorialTopic,
+        tutorialDescription,
+        user_id: user,
+   }
+   
+   const configObj ={
+    method: "POST",
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
+    body:JSON.stringify(newTutorial),
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch(`http://localhost:3000/tutorials`, configObj)
+        .then((resp) => resp.json())
+        .then((tutorial) => {addNewTutorial(tutorial)});
+    };
+
+   
     return (
         <div>
-            <Typography></Typography>
+            <br></br>
+            <br/>
+
+            <h1>Create a Tutorial</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="tutorialtitle">Tutorial Title</label>
+                    <input
+                        id="tutorialtitle"
+                        type="text"
+                        placeholder="Title of Tutorial"
+                        name="tutorialtitle"
+                        value={tutorialTitle}
+                        onChange ={(e)=> setTutorialTitle(e.target.value)}
+                        />
+                <label htmlFor="tutorialtopic">Tutorial Topic</label>
+                    <input 
+                        id="tutorialTopic"
+                        type="text"
+                        placeholder="Topic of Tutorial"
+                        name="tutorialTopic"
+                        value={tutorialTopic}
+                        onChange={(e)=>setTutorialTopic(e.target.value)}
+                    />
+                <label htmlFor="tutorialdescription">Tutorial Description</label>
+                    <input 
+                        id="tutorialDescription"  
+                        type="text"
+                        placeholder="Description of Tutorial"
+                        name="tutorialDescription"  
+                        value={tutorialDescription}
+                        onChange={(e)=>setTutorialDescription(e.target.value)}
+                    />
+                <Button 
+                    type="submit" 
+                    variant="contained" 
+                    color="warning">
+                        Create tutorial
+                </Button>
+            </form>
         </div>
     )
 }
-export default CreateTutorials
+export default CreateTutorials;
--- client/src/Tutorials.js
+++ client/src/Tutorials.js
@@ -4,23 +4,63 @@ import CreateTutorials from "./Create_Tutorials"
 import ViewTutorials from "./View_Tutorials"
 
 function Tutorials () {
-   //set Tutorials in state
+
+    //set Tutorials in state
     const [tutorials, setTutorials] = useState([]);
 
-    //get a list of tutorials
+//get a list of tutorials
     useEffect (
         () => {
             fetch(`http://localhost:3000/tutorials`)
             .then((resp)=> resp.json())
-            .then((tutorial)=>setTutorials(tutorial))
+            .then((tutorials)=>setTutorials(tutorials))
+        },[]
+    );
+//Add a new Tutorial - CREATE -
+        const addNewTutorial = (tutorial) => {
+            setTutorials([...tutorials, tutorial]);
         }
-    )
+//Handle the Edit - UPDATE -
+        function handleUpdateTutorial(updatedTutorial) 
+        {
+            const updatedTutorials = tutorials.map((tutorial) =>{
+                if (tutorial.id === updatedTutorial.id) {
+                    return updatedTutorial;
+                }
+                else {
+                    return tutorial;
+                }
+            });
+            setTutorials(updatedTutorials);
+        }
+//delete a tutorial - DELETE -
+        function deleteTutorial (id) {
+            const updatedTutorials = tutorials.filter((tutorial)=>
+            tutorial.id !== id);
+            setTutorials(updatedTutorials);
+        }
+
     return (
         <div>
-           <Typography variant="h2" color="black">View Tutorials</Typography>
-            
-            <ViewTutorials tutorials={tutorials} />
-            <CreateTutorials />
+            <Typography 
+                    variant="h2" 
+                    color="black">
+                        View Tutorials
+                </Typography>
+            {
+                tutorials.map((tutorial) => (
+                    <ViewTutorials 
+                        key={tutorial.id} 
+                        username={tutorial.username}
+                        password={tutorial.password_digest}
+                        deleteTutorial={deleteTutorial}
+                        onUpdateTutorial={handleUpdateTutorial}
+                        tutorials={tutorials}
+                        tutorial={tutorial}
+                        />
+                ))
+            }
+            <CreateTutorials addNewTutorial={addNewTutorial} tutorials={tutorials}/>
 
         </div>
     )
