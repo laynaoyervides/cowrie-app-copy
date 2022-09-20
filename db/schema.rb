fb13368fdb1d043cd5cacb4b9d6d29a87c028110
Layna Oyervides <laynaoyervides@gmail.com>
2022-09-20 14:42:49 -0600

    adding email to admins table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_24_235329) do
+ActiveRecord::Schema.define(version: 2022_09_20_203923) do
 
   create_table "admins", force: :cascade do |t|
     t.string "username"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_08_24_235329) do
     t.string "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   create_table "artworks", force: :cascade do |t|
